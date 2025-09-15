576e1c23ad65c1eabd4784690f5976d5121ab4fd
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-16 01:45:31 +0200

    Update NotificationStatus.java

--- src/main/java/com/bugtracker/bugtracker/notification/model/NotificationStatus.java
+++ src/main/java/com/bugtracker/bugtracker/notification/model/NotificationStatus.java
@@ -1,21 +1,9 @@
 package com.bugtracker.bugtracker.notification.model;
 
-/**
- * Enumeration for notification status.
- */
+
 public enum NotificationStatus {
-    /**
-     * Notification is pending to be sent
-     */
+
     PENDING,
-    
-    /**
-     * Notification has been successfully sent
-     */
     SENT,
-    
-    /**
-     * Notification failed to send
-     */
     FAILED
 } 
