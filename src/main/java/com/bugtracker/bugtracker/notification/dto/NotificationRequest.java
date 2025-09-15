9086ba856ce344210786a77aa46dc0bb02cc7a59
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-16 01:43:53 +0200

    Update NotificationRequest.java

--- src/main/java/com/bugtracker/bugtracker/notification/dto/NotificationRequest.java
+++ src/main/java/com/bugtracker/bugtracker/notification/dto/NotificationRequest.java
@@ -9,47 +9,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Data Transfer Object for creating notification requests.
- * Used by the main Bug Tracker application to request notifications.
- */
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NotificationRequest {
 
-    /**
-     * Type of notification to send
-     */
+ 
     @NotNull(message = "Notification type is required")
     private NotificationType type;
 
-    /**
-     * Recipient email address or phone number
-     */
+ 
     @NotBlank(message = "Recipient is required")
     private String recipient;
 
-    /**
-     * Subject of the notification
-     */
     @NotBlank(message = "Subject is required")
     private String subject;
 
-    /**
-     * Message content
-     */
     @NotBlank(message = "Message is required")
     private String message;
 
-    /**
-     * Bug ID this notification relates to (optional)
-     */
     private Long bugId;
 
-    /**
-     * User ID this notification is for (optional)
-     */
+ 
     private Long userId;
 } 
