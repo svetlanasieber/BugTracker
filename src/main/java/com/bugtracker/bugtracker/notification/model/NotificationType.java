6a91720eba0ae5eb1801e33296cb05644024192a
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-09-16 01:46:06 +0200

    Update NotificationType.java

--- src/main/java/com/bugtracker/bugtracker/notification/model/NotificationType.java
+++ src/main/java/com/bugtracker/bugtracker/notification/model/NotificationType.java
@@ -1,21 +1,9 @@
 package com.bugtracker.bugtracker.notification.model;
 
-/**
- * Enumeration for different types of notifications.
- */
 public enum NotificationType {
-    /**
-     * Email notification
-     */
+
     EMAIL,
-    
-    /**
-     * SMS notification (simulated)
-     */
     SMS,
-    
-    /**
-     * Push notification
-     */
+
     PUSH
 } 
