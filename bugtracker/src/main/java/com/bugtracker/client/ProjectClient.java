8d36598deac61b0f3cf92a3d28737ba9a3395557
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-10-31 02:37:29 +0100

    Add files via upload

--- bugtracker/src/main/java/com/bugtracker/client/ProjectClient.java
+++ bugtracker/src/main/java/com/bugtracker/client/ProjectClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "project-service", url = "http:
+@FeignClient(name = "project-service", url = "http://localhost:8081/api/projects")
 public interface ProjectClient {
 
     @GetMapping
--- bugtracker/src/main/java/com/bugtracker/client/dto/ProjectCreateRequest.java
+++ bugtracker/src/main/java/com/bugtracker/client/dto/ProjectCreateRequest.java
@@ -19,6 +19,7 @@ public class ProjectCreateRequest {
     private ProjectType projectType;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+    @Builder.Default
     private boolean isActive = true;
     private Long createdByUserId;
     private String createdByUsername;
