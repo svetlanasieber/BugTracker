1ddfbe5c79c77bb71a9bf67a03890c07ffaa4e6d
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-10-31 01:30:01 +0100

    Add files via upload

---
+++ bugtracker/src/main/java/com/bugtracker/user/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.bugtracker.user.repository;
+
+import com.bugtracker.user.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+} 
---
+++ bugtracker/src/main/java/com/bugtracker/user/service/UserService.java
@@ -0,0 +1,45 @@
+package com.bugtracker.user.service;
+
+import com.bugtracker.web.dto.PasswordChange;
+import com.bugtracker.web.dto.ProfileEdit;
+import com.bugtracker.web.dto.UserRegister;
+import com.bugtracker.user.model.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface UserService {
+    User registerUser(UserRegister userRegister);
+    User createUser(UserRegister userRegister);
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+    List<User> findAllUsers();
+    void changeUserRole(Long userId, String roleName);
+    boolean existsByEmail(String email);
+    
+   
+    User findByUsername(String username);
+    
+
+    List<User> findAll();
+    
+  
+    User updateUser(User user);
+    
+
+    User updateUserProfile(String username, ProfileEdit profileEdit);
+    boolean changeUserPassword(String username, PasswordChange passwordChange);
+    User getCurrentUserWithStats(String username);
+    
+
+    String resetAdminUser();
+
+    Map<String, Object> getSystemDebugInfo();
+    
+
+    boolean isUserAdmin(String username);
+    
+
+    User createUserWithRoles(String firstName, String lastName, String email, String password, List<String> roleNames);
+} 
