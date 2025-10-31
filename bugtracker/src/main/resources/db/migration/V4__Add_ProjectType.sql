9464a6707da3300fe85db0932390bda404ce19c2
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-10-31 01:44:30 +0100

    Delete bugtracker/src/main/resources directory

--- bugtracker/src/main/resources/db/migration/V4__Add_ProjectType.sql
+++
@@ -1,5 +0,0 @@
--- Add project_type column to projects table
-ALTER TABLE projects ADD COLUMN IF NOT EXISTS project_type VARCHAR(50) AFTER description;
-
--- Update existing projects to have a default type (can be modified later by users)
-UPDATE projects SET project_type = 'SOFTWARE' WHERE project_type IS NULL; 
