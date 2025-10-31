c7d488abc73226e0c265ed48ee920b0c1ea4639e
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-01 00:24:31 +0100

    Add files via upload

--- bugtracker/src/main/java/com/bugtracker/user/model/User.java
+++ bugtracker/src/main/java/com/bugtracker/user/model/User.java
@@ -93,7 +93,6 @@ public class User {
     @Builder.Default
     private Set<Comment> comments = new HashSet<>();
 
-    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
