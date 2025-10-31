a6bda3a02aef791033e66e7db674d05c21c457e3
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-10-31 01:30:19 +0100

    Add files via upload

---
+++ bugtracker/src/main/java/com/bugtracker/util/EnumUtils.java
@@ -0,0 +1,33 @@
+package com.bugtracker.util;
+
+import com.bugtracker.model.enums.BugPriority;
+import com.bugtracker.model.enums.BugStatus;
+import com.bugtracker.model.enums.LogLevel;
+import com.bugtracker.model.enums.ProjectType;
+
+public class EnumUtils {
+
+    private EnumUtils() {
+        
+    }
+
+    
+    public static boolean canTransitionTo(BugStatus currentStatus, BugStatus targetStatus) {
+        return currentStatus.canTransitionTo(targetStatus);
+    }
+    
+    
+    public static int getRecommendedResolutionTimeInHours(BugPriority priority) {
+        return priority.getRecommendedResolutionTimeInHours();
+    }
+    
+    
+    public static String getLogLevelCssClass(LogLevel level) {
+        return level.getCssClass();
+    }
+    
+    
+    public static String getProjectTypeDisplayName(ProjectType type) {
+        return type.getDisplayName();
+    }
+} 
