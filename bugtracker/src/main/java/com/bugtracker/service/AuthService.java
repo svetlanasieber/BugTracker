e9281959fbdceed752b9616f532bb7780ec8e0c6
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-10-31 01:29:41 +0100

    Add files via upload

---
+++ bugtracker/src/main/java/com/bugtracker/service/AuthService.java
@@ -0,0 +1,13 @@
+package com.bugtracker.service;
+
+public interface AuthService {
+
+    
+    String getCurrentUsername();
+
+    
+    Long getCurrentUserId();
+
+    
+    boolean isCurrentUserAdmin();
+} 
