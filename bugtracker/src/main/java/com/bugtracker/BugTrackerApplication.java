811aba66548694c24703aaff310960bbcd091f7c
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-10-31 02:43:05 +0100

    Add files via upload

--- bugtracker/src/main/java/com/bugtracker/BugTrackerApplication.java
+++ bugtracker/src/main/java/com/bugtracker/BugTrackerApplication.java
@@ -1,4 +1,4 @@
-package com.bugtracker.bugtracker;
+package com.bugtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
