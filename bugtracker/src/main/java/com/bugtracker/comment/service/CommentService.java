cc1afe09212bd3ae64ff38b6ba23db6c17d8c97c
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-10-31 01:26:23 +0100

    Add files via upload

---
+++ bugtracker/src/main/java/com/bugtracker/comment/service/CommentService.java
@@ -0,0 +1,42 @@
+package com.bugtracker.comment.service;
+
+import com.bugtracker.comment.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface CommentService {
+    
+    Comment createComment(Long bugId, Long authorId, String content);
+    
+    Comment getComment(Long commentId);
+    
+    List<Comment> getCommentsByBugId(Long bugId);
+    
+    Page<Comment> getCommentsByBugId(Long bugId, Pageable pageable);
+    
+    Comment updateComment(Long commentId, String content);
+    
+    void deleteComment(Long commentId);
+    
+    void deleteAllCommentsForBug(Long bugId);
+    
+    
+    
+    
+    Comment createCommentWithCurrentUser(Long bugId, String content, String username);
+    
+    
+    Comment updateCommentWithAuthorization(Long commentId, String content, String username);
+    
+    
+    void deleteCommentWithAuthorization(Long commentId, String username);
+    
+    
+    boolean isUserAuthorizedToModifyComment(Long commentId, String username);
+    
+    
+    Comment createCommentWithScreenshot(Long bugId, String content, MultipartFile screenshot, String username);
+} 
