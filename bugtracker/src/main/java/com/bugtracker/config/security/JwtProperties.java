9138395511fd51951285708a7de8e11773f7b412
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-01 18:37:43 +0100

    Update JwtProperties.java

--- bugtracker/src/main/java/com/bugtracker/config/security/JwtProperties.java
+++ bugtracker/src/main/java/com/bugtracker/config/security/JwtProperties.java
@@ -3,6 +3,7 @@ package com.bugtracker.config.security;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "jwt")
+    
 public record JwtProperties(
     String secretKey,
     long expirationTimeMs
