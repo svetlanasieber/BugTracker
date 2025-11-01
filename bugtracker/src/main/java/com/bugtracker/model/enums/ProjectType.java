9eed64bc9a9e23fde8514e1154e195879128ae39
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-01 18:40:14 +0100

    Update ProjectType.java

--- bugtracker/src/main/java/com/bugtracker/model/enums/ProjectType.java
+++ bugtracker/src/main/java/com/bugtracker/model/enums/ProjectType.java
@@ -3,26 +3,12 @@ package com.bugtracker.model.enums;
 public enum ProjectType {
     
     SOFTWARE("Software Development"),
-    
-    
     HARDWARE("Hardware Development"),
-    
-    
     MOBILE("Mobile Application"),
-    
-    
     WEB("Web Application"),
-    
-    
     DESKTOP("Desktop Application"),
-    
-    
     INTEGRATION("Integration Project"),
-    
-    
     RESEARCH("Research & Development"),
-    
-    
     OTHER("Other");
     
     private final String displayName;
