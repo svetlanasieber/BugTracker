7b5faadd9708ee7be6f0931d731a4bc6bb32a544
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-01 01:50:07 +0100

    Add files via upload

--- project-service/src/main/java/com/bugtracker/projectservice/service/ProjectServiceImpl.java
+++ project-service/src/main/java/com/bugtracker/projectservice/service/ProjectServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
