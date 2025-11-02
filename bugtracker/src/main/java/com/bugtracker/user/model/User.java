76c6283fb4245f961c53f10545cf9e61495b9994
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-11-02 17:58:51 +0100

    Update User.java

--- bugtracker/src/main/java/com/bugtracker/user/model/User.java
+++ bugtracker/src/main/java/com/bugtracker/user/model/User.java
@@ -51,11 +51,14 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+    
     @Column(name = "username", unique = true)
     private String username;
+    
     @Enumerated(EnumType.STRING)
     @Column(name = "team")
     private TeamRole team;
+    
     @Column(name = "profile_image")
     private String profileImage;
 
@@ -77,12 +80,15 @@ public class User {
     @Column(name = "is_active")
     @Builder.Default
     private boolean isActive = true;
+    
     @OneToMany(mappedBy = "assignedTo", cascade = CascadeType.ALL)
     @Builder.Default
     private Set<Bug> assignedBugs = new HashSet<>();
+    
     @OneToMany(mappedBy = "reporter", cascade = CascadeType.ALL)
     @Builder.Default
     private Set<Bug> reportedBugs = new HashSet<>();
+    
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
     @Builder.Default
     private Set<Comment> comments = new HashSet<>();
@@ -99,4 +105,5 @@ public class User {
     public int hashCode() {
         return getClass().hashCode();
     }
+
 } 
